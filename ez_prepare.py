4c42acfa3f357766addb2f0db8ffd5753459168d
Esayas Asefa <esayas.t.asefa@gmail.com>
2023-06-28 12:09:12 -0500

    ez_explore

--- ez_prepare.py
+++ ez_prepare.py
@@ -44,3 +44,29 @@ def remove_stopwords(text, extra_words=[], exclude_words=[]):
     filtered_words = [word for word in words if word not in stopwords_ls]
     return ' '.join(text)
 
+def represent_text(texts):
+    # Create TF-IDF vectorizer
+    vectorizer = TfidfVectorizer(tokenizer=preprocess_text)
+
+    # Fit and transform the text data
+    tfidf_matrix = vectorizer.fit_transform(texts)
+
+    return tfidf_matrix
+
+'''# Example usage
+document1 = "This is an example document for text preprocessing."
+document2 = "Text representation is important for NLP analysis."
+
+# Preprocess the text
+preprocessed_doc1 = preprocess_text(document1)
+preprocessed_doc2 = preprocess_text(document2)
+
+print("Preprocessed Document 1:", preprocessed_doc1)
+print("Preprocessed Document 2:", preprocessed_doc2)
+
+# Represent the text using TF-IDF
+texts = [document1, document2]
+tfidf_matrix = represent_text(texts)
+
+print("TF-IDF Matrix:")
+print(tfidf_matrix.toarray())'''
